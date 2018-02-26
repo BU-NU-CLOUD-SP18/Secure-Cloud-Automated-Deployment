32c5f9e5ec6bbd69107a9a4fbd12769b704c2529
Pei Jia <peijia@dhcp-wifi-8021x-155-41-125-103.bu.edu>
2018-02-25 20:42:32 -0500

    add keylime bash script

---
+++ keylime-install/keylime-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd
+
+mkdir tpm
+mkdir keylime
+
+cd tpm
+git clone https://github.com/mit-ll/tpm4720-keylime.git
+cd ../keylime
+git clone https://github.com/mit-ll/python-keylime.git
+
+cd
+sudo yum update
+sudo yum install -y openssl-devel libtool gcc automake
+
+cd tpm/tpm4720/libtpm
+./comp-chardev.sh
+sudo make install
+
+cd ../scripts
+chmod +x install-centos.sh
+./install-centos.sh
+
+/usr/local/bin/init_tpm_server
+/usr/local/bin/tpm_serverd
+
+cd ~/keylime/python-keylime/
+yum install -y python-devel python-setuptools python-tornado python-m2crypto python-zmq
+sudo python setup.py install
