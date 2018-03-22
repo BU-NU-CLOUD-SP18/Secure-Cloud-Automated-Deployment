1b4fe474d33af47f88aad57488c921bc092f1514
leonjia0112 <leon.jia.q@gmail.com>
2018-03-21 21:51:08 -0400

    Update hil_installer.sh

--- hil-install/hil_installer.sh
+++ hil-install/hil_installer.sh
@@ -6,7 +6,7 @@ sudo yum install epel-release bridge-utils  gcc  httpd  ipmitool libvirt \
 libxml2-devel  libxslt-devel  mod_wsgi net-tools python-pip python-psycopg2 \
 python-virtinst python-virtualenv qemu-kvm telnet vconfig virt-install
 
-cd hil
+cd ~/hil
 
 cp examples/hil.cfg.dev-no-hardware hil.cfg
 export HIL_ENDPOINT=http://127.0.0.1:5000
