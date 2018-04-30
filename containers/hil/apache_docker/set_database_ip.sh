a1e41d410df3d876205499b1a3f564f5ee2495c2
leonjia0112 <leon.jia.q@gmail.com>
2018-04-30 18:06:57 -0400

    Update set_database_ip.sh

--- containers/hil/apache_docker/set_database_ip.sh
+++ containers/hil/apache_docker/set_database_ip.sh
@@ -4,5 +4,5 @@ if [ -z "$1" ]
 then
       echo "please input ip address of keylime verifier and regisrar as arguemnet"
 else
-      sudo sed -i 's|uri = postgresql://hil:12345@172.18.0.20:5432/hil|uri = postgresql://hil:12345@'$1'/hil|' /etc/hil.cfg
+      sudo sed -i 's|uri = postgresql://hil:12345@0.0.0.0:5432/hil|uri = postgresql://hil:12345@'$1'/hil|' /etc/hil.cfg
 fi
